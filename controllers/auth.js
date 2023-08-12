e1dc94e6b7f016e9c4021350a089185e85ac20d9
JESSICA LEE <jessicalee@macbook-pro.mynetworksettings.com>
2023-08-12 16:16:07 -0700

    chore: fix errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,8 +11,6 @@ export const signup = async (req, res) => {
             email,
             password,
             friends,
-            location,
-            occupation,
         } = req.body;
 
         const salt = await bcrypt.genSalt();
@@ -23,12 +21,8 @@ export const signup = async (req, res) => {
             lastName,
             email,
             password: passwordHash,
-            picturePath,
             friends,
-            location,
-            occupation,
-            viewedProfile: Math.floor(Math.random() * 10000),
-            impressions: Math.floor(Math.random() * 10000),
+
         });
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
